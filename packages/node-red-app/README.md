e8f0baf26c9e202193574588e91259dffbce9f2b
Boonyarit Wiriyagulpat <boonyarit@ekoapp.com>
2022-02-28 02:13:42 +0700

    feat(main-app): add main application run by MoleculerJS

---
+++ packages/node-red-app/README.md
@@ -0,0 +1,15 @@
+# node-red-app
+
+This application was generated with [Nx](https://nx.dev).
+
+## Building
+
+Run `nx build node-red-app` to build the application.
+
+## Starting
+
+Run `nx start node-red-app` to start/run the application.
+
+## Running unit tests
+
+Run `nx test node-red-app` to execute the unit tests via [Jest](https://jestjs.io).
---
+++ packages/node-red-app/jest.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  displayName: 'node-red-app',
+  preset: '../../jest.preset.js',
+  globals: {
+    'ts-jest': {
+      tsconfig: '<rootDir>/tsconfig.spec.json',
+    },
+  },
+  transform: {
+    '^.+\\.[tj]s$': 'ts-jest',
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../coverage/packages/node-red-app',
+};
